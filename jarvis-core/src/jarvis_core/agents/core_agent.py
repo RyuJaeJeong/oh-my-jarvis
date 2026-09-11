fb991745c116f2500d3a550f8e138012541f2597
DGB\3240133 <fb0982@naver.com>
2026-09-11 17:27:54 +0900

    first commit

---
+++ jarvis-core/src/jarvis_core/agents/core_agent.py
@@ -0,0 +1,29 @@
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+
+API_KEY = "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"
+
+client = ChatNVIDIA(
+    model="google/gemma-4-31b-it",
+    api_key=API_KEY,
+    temperature=0.5,
+
+    top_p=1,
+
+    max_completion_tokens=1024,
+)
+
+lc_messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant.",
+    },
+    {
+        "role": "user",
+        "content": "What is 17 * 23?",
+    },
+]
+
+response = client.invoke(lc_messages)
+if response.additional_kwargs and "reasoning_content" in response.additional_kwargs:
+    print(response.additional_kwargs["reasoning_content"])
+print(response.content)
